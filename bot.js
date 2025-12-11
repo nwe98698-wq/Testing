58bbc789db7116a69d81bc7697f714cf0ab03bb3
nwe98698-wq <nwe98698@gmail.com>
2025-12-12 00:00:31 +0700

    Delete bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-    user_sessions[user_id] = {
-        'step': 'main',
-        'phone': '',
-        'password': '',
-        'platform': 'ck',
-        'logged_in': False,
-        'api_instance': None,
-        'current_game_type': 'WINGO_1MIN'  # NEW: Default game type
-    }
